cd534ab46acc9c6f9dbd84c4492bdf4227066ae2
DominicB-yte <53456554+DominicB-yte@users.noreply.github.com>
2020-11-27 22:32:40 +1300

    Testing WIP

---
+++ TexasRangers/TexasRangers.UITest/AppInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TexasRangers.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android
+                    .InstalledApp("com.companyname.texasrangers")
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+            else { return ConfigureApp.iOS.StartApp(); }
+        }
+    }
+}
